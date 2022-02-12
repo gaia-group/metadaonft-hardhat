97a15314c458cee98328decfbcbfb6deb13b4425
archit3c <boomerclub@protonmail.com>
2022-02-12 02:57:33 -0700

    Updates deploy args

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,12 +15,21 @@ async function main() {
   console.log('Account balance:', (await deployer.getBalance()).toString())
 
   const MetaDaoNft = await ethers.getContractFactory('MetaDaoNft')
-  const chancellor = ['0x32BF741D6DF2C00A0687b834FeC84D2A2B80388c', '0x25d53e88ae482e6612bed27d040b370c7e09838c']
-  const archit3c = '0x25d53e88ae482e6612bed27d040b370c7e09838c'
+  const chancellor = '0x32BF741D6DF2C00A0687b834FeC84D2A2B80388c'
+  const archit3c = '0x25d53e88ae482e6612bEd27d040B370c7e09838c'
   const founders = [archit3c, chancellor]
   const artist = '0x627137FC6cFa3fbfa0ed936fB4B5d66fB383DBE8'
+  const staff = {
+    ThePokeMongi: '0x367c9122748a56e3174df6a2c6bcc9d634fd2bea',
+    '0xYxussef7!': '0xFBA6cCdf60c712Bf96c094D989DDf412d1559A62',
+    'F-14 Tomcat': '0xc383039f20d6f438c60782cb7a04ec18dab5b66e',
+    nero: '0x84B8Da634d034Ff8067503CEA37828c77A9CBEab',
+    Ocean: '0x914efE0Cb888791163ABa4a5c9CE03DA349E34d7',
+    domruby: '0x0ec88a8b2973B21E38F8c46A6CafAdE2514DF73c',
+    PRD: '0x614672b1df0DA50D65472222C610980f86BE3965',
+  }
   const baseURI = 'ipfs://QmQscUmBUYcbRsJmHueLAYBM5egSoS2iKkUbCKteFC68gd/'
-  const token = await MetaDaoNft.deploy(founders, artist, baseURI)
+  const token = await MetaDaoNft.deploy(founders, artist, Object.values(staff), baseURI)
   await token.deployed()
 
   console.log('Token address:', token.address)
